245075231ab0927fc9d1045048d7f9f7983c4276
Marcel Kummer <tanuva@posteo.de>
2022-10-03 13:44:58 +0200

    Initial commit: can periodically read a file
    In preparation for publishing the data via MQTT.

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::{env, process, path::Path, fs, time::Duration};
+
+struct Options {
+    device_name: String
+}
+
+fn parse_args(args: Vec<String>) -> Result<Options, &'static str> {
+    if args.len() != 2 {
+        return Err("Expected exactly 1 argument: the device to read from");
+    }
+
+    let options = Options {
+        device_name: args[1].to_string()
+    };
+
+    return Ok(options);
+}
+
+fn handle_data(data: &str) {
+    println!("Received data: {}", data);
+}
+
+fn read_loop(device_path: &Path) {
+    loop {
+        match fs::read_to_string(device_path) {
+            Ok(data) => handle_data(&data.trim()),
+            Err(err) => {
+                eprintln!("Cannot open device for reading: {}", err);
+            },
+        };
+
+        std::thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let options = parse_args(args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    println!("d0bby reading from {}", options.device_name);
+    read_loop(&Path::new(&options.device_name));
+}
